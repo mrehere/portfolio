4ae288bd067db481263e1b3eff8b647d3408f0bf
Majedur Rahman <md.majedur97@gmail.com>
2024-11-11 14:15:12 -0330

    footer-created-with-icons-hoover-effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./styles/partials/_global.scss";
 import "./App.scss";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 
 import Nav from "./Components/Nav/Nav";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <main className="viewBox">
       <Nav />
+      <Footer />
     </main>
   );
 }
