38a19b6a4ddb5e72928d2e8585ed89a00d6cddd3
Majedur Rahman <md.majedur97@gmail.com>
2024-11-11 21:49:44 -0330

    home-page-created

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 
 import Nav from "./Components/Nav/Nav";
 import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <main className="viewBox">
-      <Nav />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </main>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.scss";
+function Home() {
+  return (
+    <main className="home">
+      <h1>hello from home</h1>
+    </main>
+  );
+}
+
+export default Home;
