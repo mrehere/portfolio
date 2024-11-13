19bb50609e242d717e26d55fe4f1871a65485cb9
Majedur Rahman <md.majedur97@gmail.com>
2024-11-12 21:27:49 -0330

    toggled-mobile-menu-structure-added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,18 +1,54 @@
 import "./Nav.scss";
 import hamburgerIcon from "../../assets/icons/hamburger.svg";
+import closeIcon from "../../assets/icons/close.svg";
+import { useState } from "react";
 
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <main className="nav">
       <section className="nav__mobile-nav">
         <div className="nav__mobile-name">majedur-rahman</div>
 
-        <img
-          src={hamburgerIcon}
-          alt="hamburger-icon"
-          className="nav__hamburger"
-        />
+        {!isMenuOpen && (
+          <img
+            src={hamburgerIcon}
+            alt="hamburger-icon"
+            className="nav__hamburger"
+            onClick={toggleMenu}
+          />
+        )}
+
+        {isMenuOpen && (
+          <img
+            src={closeIcon}
+            alt="hamburger-icon"
+            className="nav__close"
+            onClick={toggleMenu}
+          />
+        )}
       </section>
+
+      {isMenuOpen && (
+        <section className="nav__menu">
+          <div className="nav__menu-content">
+            <ul>
+              <li>_home</li>
+              <li>_about-me</li>
+              <li>_projects</li>
+              <li>_contact-me</li>
+            </ul>
+            <button onClick={toggleMenu} className="nav__menu-close">
+              Close
+            </button>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
