23cd837194e264789383cc72f70f43f7c0298660
Majedur Rahman <md.majedur97@gmail.com>
2024-11-13 12:23:00 -0330

    min-height-added-to-app

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,16 @@ import "./Home.scss";
 function Home() {
   return (
     <main className="home">
-      <h1>hello from home</h1>
+      <div className="home__head">
+        <p className="home__hello">Hello, I am</p>
+        <h1 className="home__name">Majedur Rahman</h1>
+        <p className="home__designation">{`> Full-stack developer`}</p>
+      </div>
+
+      <div className="home__info">
+        <p className="home__infoTitle">{`// find my profile on Github:`}</p>
+        <p className="home__gitLink">{`const githubLink = https://github.com/mrehere`}</p>
+      </div>
     </main>
   );
 }
