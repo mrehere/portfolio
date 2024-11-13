fbbb23ee84c84ba8b50b55a61de096d19254d723
Majedur Rahman <md.majedur97@gmail.com>
2024-11-13 12:57:07 -0330

    mobile-home-implemented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import "./Home.scss";
 function Home() {
   return (
     <main className="home">
+      <div className="home__gradient1"> </div>
       <div className="home__head">
         <p className="home__hello">Hello, I am</p>
         <h1 className="home__name">Majedur Rahman</h1>
@@ -9,8 +10,25 @@ function Home() {
       </div>
 
       <div className="home__info">
-        <p className="home__infoTitle">{`// find my profile on Github:`}</p>
-        <p className="home__gitLink">{`const githubLink = https://github.com/mrehere`}</p>
+        <p className="home__infoTitle">{`/ / find my profile on Github:`}</p>
+        <p className="home__gitLink">
+          <span className="home__gitLink home__gitLink-const">const</span>
+
+          <span className="home__gitLink home__gitLink-variable">
+            {" "}
+            githubLink{" "}
+          </span>
+          <span className="code-operator"> = </span>
+          <a
+            href="https://github.com/mrehere"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="home__gitLink home__gitLink-link">
+              "https://github.com/mrehere"
+            </span>
+          </a>
+        </p>
       </div>
     </main>
   );
