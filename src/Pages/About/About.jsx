1c289181eff3ba457e82738d6d520d9943e0f2f2
Majedur Rahman <md.majedur97@gmail.com>
2024-11-15 13:15:49 -0330

    sub-sectionIcons-Styled-&-created

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 import "./About.scss";
+
+import arrow from "../../assets/icons/arrow.svg";
+import experience from "../../assets/icons/about/experience.svg";
+import hs from "../../assets/icons/about/hard-skills.svg";
+import ss from "../../assets/icons/about/soft-skills.svg";
+import ed from "../../assets/icons/about/education.svg";
+import hobby from "../../assets/icons/about/hobby.svg";
+import values from "../../assets/icons/about/heart.svg";
+
 function About() {
-  const [activeMenu, setActiveMenu] = useState(null);
+  const [activeMenu, setActiveMenu] = useState(0);
 
   const handleMenu = (index) => {
     setActiveMenu((prev) => (prev === index ? null : index));
@@ -11,59 +20,65 @@ function About() {
       <h2 className="about__me">_about-me</h2>
 
       <div className="about__mobile-nav">
+        {/* professional-info */}
         <div
           onClick={() => handleMenu(0)}
           className={`about__nav-menu ${
             activeMenu === 0 ? "about__nav-menu-active" : ""
           }`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#D9D9D9"
-          >
-            <path d="M400-280v-400l200 200-200 200Z" />
-          </svg>
+          <img src={arrow} alt="arrow" className="about__arrow" />
           professional-info
         </div>
 
+        {activeMenu === 0 && (
+          <div className="about__menu-item-container">
+            {/* <div className="about__menu-item">
+              <img
+                src={experience}
+                alt="pro-icon"
+                className="about__item-img"
+              />
+              experience
+            </div> */}
+            <div className="about__menu-item">
+              <img src={hs} alt="pro-icon" className="about__item-img" />
+              hard-skills
+            </div>
+            <div className="about__menu-item">
+              <img src={ss} alt="pro-icon" className="about__item-img" />
+              soft-skills
+            </div>
+            <div className="about__menu-item">
+              <img src={ed} alt="pro-icon" className="about__item-img" />
+              education{" "}
+            </div>
+          </div>
+        )}
+
+        {/* personal info */}
         <div
           onClick={() => handleMenu(1)}
           className={`about__nav-menu ${
             activeMenu === 1 ? "about__nav-menu-active" : ""
           }`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#D9D9D9"
-          >
-            <path d="M400-280v-400l200 200-200 200Z" />
-          </svg>
-          education-info
+          <img src={arrow} alt="arrow" className="about__arrow" />
+          personal-info
         </div>
 
-        <div
-          onClick={() => handleMenu(2)}
-          className={`about__nav-menu ${
-            activeMenu === 2 ? "about__nav-menu-active" : ""
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#D9D9D9"
-          >
-            <path d="M400-280v-400l200 200-200 200Z" />
-          </svg>
-          hobbies-info
-        </div>
+        {activeMenu === 1 && (
+          <div className="about__menu-item-container">
+            <div className="about__menu-item">
+              <img src={hobby} alt="pro-icon" className="about__item-img" />
+              hobbies
+            </div>
+            <div className="about__menu-item">
+              <img src={values} alt="pro-icon" className="about__item-img" />
+              values
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
