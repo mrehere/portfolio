893d997d412e5dd03014b1e83ce210da7031dd62
Majedur Rahman <md.majedur97@gmail.com>
2024-11-18 21:54:08 -0330

    about-code-snippet-implemented

--- src/Components/Gist/Gist.jsx
+++ src/Components/Gist/Gist.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import "./Gist.scss";
 import { useEffect, useState } from "react";
+import { formatDistanceToNow } from "date-fns";
 
 import details from "../../assets/icons/details.svg";
 import close from "../../assets/icons/close.svg";
@@ -17,10 +18,11 @@ function Gist() {
   const [error, setError] = useState("");
 
   const [detailsStatus, setDetailsStatus] = useState(false);
+  const [detailsIndex, setDetailsIndex] = useState(null);
 
-  const handleDetails = () => {
-    setDetailsStatus((prev) => !prev);
-    console.log(detailsStatus);
+  const handleDetails = (index) => {
+    setDetailsIndex(detailsIndex === index ? null : index);
+    console.log(detailsIndex);
   };
   useEffect(() => {
     const fetchGists = async () => {
@@ -46,69 +48,75 @@ function Gist() {
   }, []);
 
   useEffect(() => {
-    if (data && Array.isArray(data)) {
-      const transformedSnippets = data.map((item) => {
-        const fileUrls = Object.values(item.files).map((file) => file.raw_url);
-
-        return {
-          raw_urls: fileUrls,
-          description: item.description || "No description available",
-        };
-      });
-
-      // Update state with transformed data
-      setSnippet(transformedSnippets);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    const fetchCode = async () => {
+    const getSnippets = async () => {
       try {
-        const response = await axios.get(
-          "https://gist.githubusercontent.com/mrehere/f5e0e532196cbafc48a7b8bd45c7afa5/raw/359c247dbb6a83f246b8bbf7ce09ca5d8957438d/posting-journal-backend.js"
-        );
-        setCode(response.data);
+        if (data && Array.isArray(data)) {
+          // Use Promise.all to handle async calls inside map
+          const transformedSnippets = await Promise.all(
+            data.map(async (item) => {
+              const fileUrls = await Promise.all(
+                Object.values(item.files).map(async (file) => {
+                  const response = await axios.get(file.raw_url);
+                  return response.data;
+                })
+              );
 
-        setLoading(false);
-      } catch (err) {
-        setError("Error fetching the code", err);
-        setLoading(false);
+              return {
+                raw_urls: fileUrls,
+                description: item.description || "No description available",
+                created_at: item.created_at,
+              };
+            })
+          );
+
+          // Update state with transformed data
+          setSnippet(transformedSnippets);
+        }
+      } catch (error) {
+        console.log("Unable to fetch snippets, error:", error);
       }
     };
 
-    fetchCode();
-  }, []);
+    getSnippets();
+  }, [data]);
 
   return (
     <section className="gist">
-      <div className="gist__details-date">
-        <p className="gist__date">date 4343</p>
-        <div onClick={handleDetails} className="gist__button-container">
-          <img
-            className="gist__img
+      {snippet.map((item, index) => (
+        <div className="gist__snippet-container" key={index}>
+          <div className="gist__details-date">
+            <p className="gist__date">
+              {formatDistanceToNow(item.created_at, { addSuffix: true })}
+            </p>
+            <div
+              onClick={() => handleDetails(index)}
+              className="gist__button-container"
+            >
+              <img
+                className="gist__img
           "
-            src={details}
-            alt=""
-          />
-          <button className="gist__button">details</button>
+                src={details}
+                alt=""
+              />
+              <button className="gist__button">details</button>
+            </div>
+          </div>
+          <pre className="gist__pre">
+            <code>{item.raw_urls}</code>
+          </pre>
+          {detailsIndex === index && (
+            <div className="gist__details-container">
+              <p className="gist__details">{item.description}</p>
+              <img
+                className="gist__close"
+                onClick={() => handleDetails(index)}
+                src={close}
+                alt=""
+              />
+            </div>
+          )}
         </div>
-      </div>
-      <pre className="gist__pre">
-        <code>{code}</code>
-      </pre>
-      {detailsStatus && (
-        <div className="gist__details-container">
-          <p className="gist__details">
-            this is just a dummy details to work with :D
-          </p>
-          <img
-            className="gist__close"
-            onClick={handleDetails}
-            src={close}
-            alt=""
-          />
-        </div>
-      )}
+      ))}
     </section>
   );
 }
